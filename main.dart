14e37ad0bbb547fc23e1307fbf837f7d957937ad
aleena02122002 <khananna919@gmail.com>
2024-01-27 18:23:33 +0500

    Time converter added

--- main.dart
+++ main.dart
@@ -2,12 +2,12 @@ import 'dart:io';
 
 String? value1, value2;
 void main() {
-  if (code()) {
+  if (lenght()) {
     stdout.write("Do you want to start again? yes/no : ");
     String? choice = stdin.readLineSync();
     switch (choice) {
       case 'yes':
-        code();
+        lenght();
       case 'no':
         break;
 
@@ -17,7 +17,56 @@ void main() {
   }
 }
 
-bool code() {
+bool lenght() {
+  print(
+      "Please Enter number \n 1) M to Km \n 2) Km to M \n 3) Ft to In \n 4) In to Ft \n 5) Cm to M \n 6) M to Cm");
+  stdout.write("Enter number: ");
+  value2 = stdin.readLineSync();
+
+  switch (value2) {
+    case '1':
+      stdout.write(" Value: ");
+      value1 = stdin.readLineSync();
+      double value3 = double.tryParse(value1!) ?? 0.0;
+      //meter_kilometer(value3);
+      print(meter_kilometer(value3));
+    case '2':
+      stdout.write(" Value: ");
+      value1 = stdin.readLineSync();
+      double value3 = double.tryParse(value1!) ?? 0.0;
+      //meter_kilometer(value3);
+      print(kilometer_meter(value3));
+    case '3':
+      stdout.write(" Value: ");
+      value1 = stdin.readLineSync();
+      double value3 = double.tryParse(value1!) ?? 0.0;
+      //meter_kilometer(value3);
+      print(feet_inches(value3));
+    case '4':
+      stdout.write(" Value: ");
+      value1 = stdin.readLineSync();
+      double value3 = double.tryParse(value1!) ?? 0.0;
+      //meter_kilometer(value3);
+      print(inches_feet(value3));
+    case '5':
+      stdout.write(" Value: ");
+      value1 = stdin.readLineSync();
+      double value3 = double.tryParse(value1!) ?? 0.0;
+      //meter_kilometer(value3);
+      print(cm_meter(value3));
+    case '6':
+      stdout.write(" Value: ");
+      value1 = stdin.readLineSync();
+      double value3 = double.tryParse(value1!) ?? 0.0;
+      //meter_kilometer(value3);
+      print(meter_cm(value3));
+    default:
+      print("Error");
+  }
+  return true;
+}
+
+bool time() {
   print(
       "Please Enter number \n 1) M to Km \n 2) Km to M \n 3) Ft to In \n 4) In to Ft \n 5) Cm to M \n 6) M to Cm");
   stdout.write("Enter number: ");
@@ -106,4 +155,54 @@ double meter_cm(double value) {
   //6
   double cm = value * 100;
   return cm;
+}
+// temprature conversion
+
+//1
+double f_to_c(double value) {
+  double celsius = (value - 32) / 1.8;
+  return celsius;
+}
+
+//2
+double c_to_f(double value) {
+  double fahrenhiet = (9 / 5) * value + 32;
+  return fahrenhiet;
+}
+// Time conversion
+
+//1
+double sec_min(double value) {
+  double minutes = value / 60;
+  return minutes;
+}
+
+//2
+double min_sec(double value) {
+  double second = value * 60;
+  return second;
+}
+
+//3
+double min_hour(double value) {
+  double second = value / 60;
+  return second;
+}
+
+//4
+double sec_hour(double value) {
+  double hour = value / 3600;
+  return hour;
+}
+
+//5
+double milli_min(double value) {
+  double min = value / 60000;
+  return min;
+}
+
+//6
+double milli_hour(double value) {
+  double ho = value / 3.6000e+6;
+  return ho;
 }
